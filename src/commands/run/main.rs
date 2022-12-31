8bfb9f59fbdeb898a8897f31c9567ae0580daba8
McKayla Washburn <mckayla@hey.com>
2022-12-30 21:14:19 -0700

    flesh out `kirbo run` a little

--- src/commands/run/main.rs
+++ src/commands/run/main.rs
@@ -1,7 +1,36 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use std::process::Command;
+
+use crate::npm;
 use crate::options::Options;
 
 pub fn main(options: Options) {
 	let args = options.remaining_args;
 
-	println!("{:?}", args);
+	let package = serde_json::from_str::<npm::PackageJson>(
+		&fs::read_to_string(PathBuf::from(&args[0]).join("package.json")).unwrap(),
+	)
+	.unwrap();
+
+	let path_env = format!(
+		"{}:{}",
+		env::current_dir()
+			.unwrap()
+			.join(&args[0])
+			.join("node_modules/.bin/")
+			.display(),
+		env::var("PATH").unwrap()
+	);
+
+	Command::new("sh")
+		.arg("-c")
+		.arg(&package.scripts[&args[1]])
+		.env("PATH", path_env)
+		.current_dir(&args[0])
+		.spawn()
+		.unwrap()
+		.wait()
+		.unwrap();
 }
